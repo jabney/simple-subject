1efc9860cfce52f2e4b8e25aaf8ed34f5e7cc221
James Abney <james.abney@gmail.com>
2017-07-18 08:10:03 -0700

    Comments

--- src/observers.ts
+++ src/observers.ts
@@ -1,5 +1,11 @@
 import { TNotifyCallback } from './notify-callback'
 
+/**
+ * A map of id -> callback for storing subscribers to a subject.
+ *
+ * @export
+ * @interface IObservers
+ */
 export interface IObservers {
   [id: number]: TNotifyCallback
 }
